bda4e0b6436f344b1f692e81ce329e5c4069754d
boy52hz <ggwp.zero@gmail.com>
2022-04-01 23:50:44 +0700

    changed fetching method to use try catch

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -1,31 +1,29 @@
 <script setup>
 import { onBeforeMount, ref } from 'vue'
-import Book from '../components/Book.vue';
+import Book from '../components/Book.vue'
 
 const books = ref([])
 
 const getUsers = async () => {
+  try {
     const res = await fetch('http://localhost:5000/books', { method: 'GET' })
-    if (res.status === 200) {
-        return await res.json()
-    } else {
-        console.log('cant fetch book')
-    }
+    return await res.json()
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 onBeforeMount(async () => {
-    const bk = await getUsers();
-    books.value = bk.book
+  const bk = await getUsers()
+  books.value = bk.book
 })
-
 </script>
- 
+
 <template>
-    <div id="user">
-        <h2>Users</h2>
-        <Book :books="books" />
-    </div>
+  <div id="user">
+    <h2>Users</h2>
+    <Book :books="books" />
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,31 +1,29 @@
 <script setup>
 import { onBeforeMount, ref } from 'vue'
-import User from '../components/User.vue';
+import User from '../components/User.vue'
 
 const users = ref([])
 
 const getUsers = async () => {
+  try {
     const res = await fetch('http://localhost:5000/users', { method: 'GET' })
-    if (res.status === 200) {
-        return await res.json()
-    } else {
-        console.log('cant fetch user')
-    }
+    return await res.json()
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 onBeforeMount(async () => {
-    const usr = await getUsers();
-    users.value = usr.user
+  const usr = await getUsers()
+  users.value = usr.user
 })
-
 </script>
- 
+
 <template>
-    <div id="user">
-        <h2>Users</h2>
-        <User :users="users" />
-    </div>
+  <div id="user">
+    <h2>Users</h2>
+    <User :users="users" />
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
