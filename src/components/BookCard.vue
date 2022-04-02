7a65b2c128339007ec2933b848896530f429a8bc
Chinnawat63130500022 <sapedpunk@hotmail.com>
2022-04-02 19:27:57 +0700

    Add Demo Home Page

---
+++ src/components/BookCard.vue
@@ -0,0 +1,66 @@
+<script setup>
+defineProps({
+  books: {
+    type: Object,
+    require: true,
+  },
+})
+</script>
+
+<template>
+  <div>
+    <h1 id="H1">BOOKS FOR RENT</h1>
+    <ul>
+      <li class="BookBox" v-for="(book, index) in books" :key="index">
+        <div class="BookCard">
+            <img class="book_img" :src="book.bImg" />
+            <br>
+                <p class="book_name">{{ book.bName }}</p>
+            <div class="addToCardBox">
+                <p class="addToCartBtn">ADD TO CART</p>
+            </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+#H1{
+    text-align: center;
+    font-family: 'Skranji', cursive;
+}
+.BookBox{
+    display: flex;
+    justify-content: space-around;
+}
+.BookCard{
+    width:25%
+}
+.book_img{
+    width: 100%;
+    padding: 8px;
+}
+.book_name{
+    font-family: 'Skranji', cursive;
+    text-align: center;
+}
+.addToCartBtn{
+    text-align: center;
+    font-family: 'Itim', cursive;
+    color: rgb(255, 255, 255);
+}
+.addToCardBox{
+    background: rgb(29, 11, 1);
+    margin: auto;
+    width: 50%;
+    border-radius: 10px;
+    padding: 5px;
+    transition: all 0.3s ease-out;
+}
+.addToCardBox:hover {
+  cursor: pointer;
+  transform: scale(1.1);
+  filter: drop-shadow(0 0 0.7rem rgba(255, 255, 255, 0.5));
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,28 @@
 <script setup>
 import { useUser } from '../stores/user.js'
+import { onBeforeMount, ref } from 'vue'
+import BookCard from '../components/BookCard.vue';
 const userStore = useUser()
+const books = ref([])
+
+const getUsers = async () => {
+  try {
+    const res = await fetch('http://localhost:5000/books', { method: 'GET' })
+    return await res.json()
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+onBeforeMount(async () => {
+  const fetchedBooks = await getUsers()
+  books.value = fetchedBooks
+})
 </script>
 
 <template>
-  <div id="home" v-if="userStore.user">
+<div>
+  <div v-if="userStore.user">
     <h2>Home</h2>
     <p>
       Hello,
@@ -16,6 +34,14 @@ const userStore = useUser()
       <a href="" @click="userStore.logout()">Logout</a>
     </p>
   </div>
+    <div id="BookCard">
+      <BookCard :books="books" />
+  </div>
+</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#BookCard{
+  background: rgba(0, 151, 19, 0.1);
+}
+</style>
