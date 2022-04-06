67246da796ddb7841740ca43ab88cb852a157b04
mintchyy <immintchyy@gmail.com>
2022-04-07 00:05:54 +0700

    add AboutUs

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,24 @@
 <script setup>
-
+import AboutUs from '../components/AboutUs.vue'
 </script>
  
 <template>
   <div id="about">
     <h2>About</h2>
+    <AboutUs />
   </div>
 </template>
  
 <style scoped>
+#about {
+  background-image: url(../assets/background-img/bg-aboutUs.jpg);
+  color: rgb(248, 246, 246);
+  background-repeat: no-repeat;
+  background-size: cover;
+  padding: 30px 120px 40px 120px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
