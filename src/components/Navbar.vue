9a25284552d9268692b8befd82545e3d914de26b
mintchyy <immintchyy@gmail.com>
2022-04-08 00:45:56 +0700

    add nav search

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -33,6 +33,12 @@ onBeforeMount(async () => {
       <li>
         <router-link :to="{ name: 'About' }">About</router-link>
       </li>
+      <li>
+        <p class="s">
+          <input name="search" id="search" type="search" />
+        </p>
+      </li>
+      <li class="iconify" data-icon="iconoir:cart-alt" data-inline="false"></li>
       <!-- NAV BAR USER IMAGE -->
       <img @click="useUser().logout()" :src="userImg" alt="user image" style="width: 50px" />
       <!-- NAV BAR USER IMAGE -->
@@ -97,5 +103,68 @@ ul.navs {
 .scrolled {
   padding: 8px 32px !important;
   opacity: 0.9;
+}
+#search {
+  -webkit-appearance: none;
+  font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+  width: 24px;
+  padding: 0 10px;
+  height: 24px;
+  font-size: 14px;
+  color: #666;
+  line-height: 24px;
+  border: 0;
+  border-radius: 50px;
+  box-shadow: 0 0 0 1px rgba(110, 0, 200, 0.5),
+    inset 0 2px 5px rgba(82, 0, 150, 0.3), 0 2px 0 rgba(255, 255, 255, 0.6);
+  position: relative;
+  z-index: 5;
+  -webkit-transition: 0.3s ease;
+  -moz-transition: 0.3s ease;
+}
+
+#search:focus {
+  outline: none;
+  width: 180px;
+}
+
+p.s {
+  z-index: 4;
+  position: relative;
+  padding: 5px;
+  line-height: 0;
+  border-radius: 100px;
+  background: #b78cdf;
+  background-image: -webkit-linear-gradient(#e2dbff, #b78cdf);
+  background-image: -moz-linear-gradient(#e2dbff, #b78cdf);
+  display: inline-block;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.6),
+    0 2px 5px rgba(186, 121, 248, 0.5);
+}
+
+p.s:hover {
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.6),
+    0 2px 3px 2px rgba(186, 121, 248, 0.5);
+}
+
+p.s:after {
+  content: "";
+  display: block;
+  position: absolute;
+  width: 5px;
+  height: 20px;
+  background: #b78cdf;
+  bottom: -10px;
+  right: -3px;
+  border-radius: 0 0 5px 5px;
+  -webkit-transform: rotate(-45deg);
+  -moz-transform: rotate(-45deg);
+  box-shadow: inset 0 -1px 0 rgbA(255, 255, 255, 0.6),
+    -2px 2px 2px rgba(95, 0, 150, 0.4);
+}
+
+p.s:hover:after {
+  box-shadow: inset 0 -1px 0 rgba(255, 255, 255, 0.6),
+    -2px 2px 2px 1px rgba(186, 121, 248, 0.5);
 }
 </style>
