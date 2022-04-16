959af79e021c56c06281e7fcbb57791117ad062a
boy52hz <ggwp.zero@gmail.com>
2022-04-17 05:27:43 +0700

    remove unnecessary css class in CartList.vue

--- src/components/CartList.vue
+++ src/components/CartList.vue
@@ -10,24 +10,37 @@ defineEmits(['return-book'])
 
 <template>
   <div class="container">
-    <h1 style=" color: black; padding-top: 20px ">Your Books</h1>
+    <h1 style="color: black; padding-top: 20px">Your Books</h1>
     <table style="width: 100%">
-      <tr >
+      <tr>
         <th>BOOK</th>
         <th>RETURN</th>
       </tr>
-  <tr v-for="(cartItem, index) in cartItems" :key="index">
+      <tr v-for="(cartItem, index) in cartItems" :key="index">
         <td>
           <li>
-           <th> <img class="bookImg" :src="cartItem.bImg" style="width: 100%;" /></th>
-          <th>  <span class="bookId"><b>book id : </b>{{ cartItem.id }} </span></th>
-            <th><span class="bookName"><b>book name </b> : {{ cartItem.bName}}</span></th>
+            <th>
+              <img class="book-img" :src="cartItem.bImg" style="width: 100%" />
+            </th>
+            <th>
+              <span class="book-id"><b>book id : </b>{{ cartItem.id }} </span>
+            </th>
+            <th>
+              <span class="book-name"
+                ><b>book name </b> : {{ cartItem.bName }}</span
+              >
+            </th>
           </li>
           <hr />
         </td>
         <td class="return-icon">
-          <img @click="$emit('return-book', cartItem)" class="remove-icon" style="height: 60px; width: 60px"
-            src="../assets/return-icon.png" alt="remove icon " />
+          <img
+            @click="$emit('return-book', cartItem)"
+            class="remove-icon"
+            style="height: 60px; width: 60px"
+            src="../assets/return-icon.png"
+            alt="remove icon "
+          />
         </td>
       </tr>
     </table>
@@ -38,17 +51,16 @@ defineEmits(['return-book'])
 table {
   border-bottom: 2px solid black;
 }
-th{
+th {
   border: 2px solid black;
 }
-.bookImg{
+.book-img {
   grid-area: 1/1;
-
 }
-.bookName{
+.book-name {
   grid-area: 2/2;
 }
-.bookId{
+.boo-id {
   grid-area: 1/2;
 }
 .container {
@@ -65,105 +77,9 @@ th{
   border: 2px solid black;
 }
 
-
 .container li {
   list-style: none;
   display: grid;
   grid-template-columns: 1fr 2fr 2fr;
- 
-}
-
-.days button {
-  font-family: 'Skranji', cursive;
-
-  background-color: rgb(116, 119, 116);
-  padding: 8px;
-  border-radius: 10em;
-}
-
-.summary {
-  font-family: 'Skranji', cursive;
-  width: 40%;
-  border-radius: 1em;
-  padding: 8px;
-  background-color: rgba(255, 255, 255, 0.69);
-  display: flex;
-  position: relative;
-  flex-direction: column;
-  align-items: center;
-  margin: 0 auto;
-}
-
-.summary button {
-  font-family: 'Skranji', cursive;
-  background-color: green;
-  padding: 8px;
-  border-radius: 2em;
-}
-
-input[type='number'] {
-  -webkit-appearance: textfield;
-  -moz-appearance: textfield;
-  appearance: textfield;
-}
-
-input[type='number']::-webkit-inner-spin-button,
-input[type='number']::-webkit-outer-spin-button {
-  -webkit-appearance: none;
-}
-
-.number-input {
-  border: 0;
-  display: inline-flex;
-}
-
-.number-input,
-.number-input * {
-  box-sizing: border-box;
-}
-
-.number-input button {
-  outline: none;
-  -webkit-appearance: none;
-  background-color: transparent;
-  border: none;
-  align-items: center;
-  justify-content: center;
-  width: 30px;
-  height: 30px;
-  cursor: pointer;
-  margin: 0;
-  position: relative;
-  box-shadow: 0px 0px 1px #474747;
-  border-radius: 80%;
-}
-
-.number-input button:before,
-.number-input button:after {
-  display: inline-block;
-  position: absolute;
-  content: '';
-  width: 1rem;
-  height: 2px;
-  background-color: #212121;
-  transform: translate(-50%, -50%);
-}
-
-.number-input button.plus:after {
-  transform: translate(-50%, -50%) rotate(90deg);
-}
-
-.number-input input[type='number'] {
-  font-family: 'Skranji', cursive;
-  max-width: 30px;
-  padding: 0.5rem;
-
-  border: none;
-  border-width: 0 2px;
-  font-size: 1rem;
-  height: 3rem;
-  font-weight: bold;
-  text-align: center;
-  color: #000000;
 }
 </style>
