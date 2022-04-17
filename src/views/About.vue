1650903838042665321a1ba6edf6e2aab23c6a31
ChisanuchaK <chisanucha.kim@mail.kmutt.ac.th>
2022-04-17 21:16:37 +0700

    ui history and about

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,8 +3,8 @@ import AboutUs from '../components/AboutUs.vue'
 </script>
  
 <template>
-  <div id="about">
-    <h1>About</h1>
+  <div class="about">
+    <h1 class="titel-about">About</h1>
 
     <div class="box">
       <div>
@@ -15,25 +15,32 @@ import AboutUs from '../components/AboutUs.vue'
 </template>
  
 <style scoped>
+h1{
+  color: white;
+  width: 100%;
+  text-align: center;
+  padding: 20px 0 0 0;
+      font-family: "Skranji";
+}
 .box {
   background-color: rgb(255, 255, 255);
-  opacity: 0.7;
+  opacity: 0.85;
   border-radius: 20px;
   background-size: cover;
   width: 50%;
   height: 50%;
+  border: 10px solid black;
+  margin: 0 auto;
+  margin-top: 5%;
 }
 
-#about {
+.about {
   background-image: url(../assets/background-img/bg-aboutUs.jpg);
   color: rgb(0, 0, 0);
   background-repeat: no-repeat;
   background-size: cover;
   padding: 30px 120px 40px 120px;
   height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 }
 
 #about h1 {
